9510b5cd451149caeb9912d60b2f96d87406fdaa
Oleksandr Bogomaz <o.v.bogomaz1985@gmail.com>
2024-07-26 21:12:33 -0400

    Hide files and directories

--- build_standalone.bat
+++ build_standalone.bat
@@ -2,6 +2,9 @@ echo exec "conda activate base" before
 python -m nuitka --standalone --follow-imports --nofollow-import-to=tkinter --windows-console-mode=disable --enable-plugin=pyqt6 --assume-yes-for-downloads IonogramViewer2.py
 rmdir /q /s IonogramViewer2.build
 ren IonogramViewer2.dist IonogramViewer2
+attrib +h /d IonogramViewer2\*.*
+attrib -h IonogramViewer2\IonogramViewer2.exe
+attrib -h /d IonogramViewer2\PyQt6
 xcopy ui IonogramViewer2\ui /e /i /h
 xcopy data IonogramViewer2\data /e /i /h
 xcopy examples IonogramViewer2\examples /e /i /h
